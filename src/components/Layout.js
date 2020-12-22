428129cd17bd204f74882ea7b89c5726f2dc4ed7
luron <joseluron@gmail.com>
2020-12-23 00:27:16 +0100

    Applied some global styles with StyledComponents

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import 'normalize.css';
+
 import Nav from './Nav';
 import Footer from './Footer';
+import GlobalStyles from '../styles/GlobalStyles';
 
 export default function Layout(props) {
   const { children } = props;
 
   return (
     <>
+      <GlobalStyles />
       <Nav />
       {children}
       <Footer />
