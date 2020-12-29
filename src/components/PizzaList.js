936474b9302776aaa4e8c537a4e19fdc3fb73c1f
luron <joseluron@gmail.com>
2020-12-29 01:45:59 +0100

    Implemented query in PizzasPage to get all the pizzas in the Sanity cms

---
+++ src/components/PizzaList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+function SinglePizza({ pizza }) {
+  return (
+    <Link to={`/pizza/${pizza.slug.current}`}>
+      <h2>
+        <span className="mark">{pizza.name}</span>
+      </h2>
+      <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+    </Link>
+  );
+}
+
+export default function PizzaList({ pizzas }) {
+  return (
+    <>
+      {pizzas.map((pizza) => (
+        <SinglePizza key={pizza.id} pizza={pizza} />
+      ))}
+    </>
+  );
+}
