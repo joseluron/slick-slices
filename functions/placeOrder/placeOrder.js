4cb85fa993b80ae3cefa45eb78d4d70da13ebf56
luron <joseluron@gmail.com>
2021-01-15 03:01:24 +0100

    Created serverless netlify 'placeOrder' function to send emails when a order is placed

---
+++ functions/placeOrder/placeOrder.js
@@ -0,0 +1,35 @@
+const nodemailer = require('nodemailer');
+
+// Create a transport for nodemailer
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+exports.handler = async (event, context) => {
+  // Test send an email
+  const info = await transporter.sendMail(
+    {
+      from: "Slick's Slices <slick@example.com>",
+      to: 'orders@example.com',
+      subject: 'New Order!!',
+      text: 'This is your order!',
+      html: `<p>Your new pizza order is here!</p>`,
+    },
+    (err) => {
+      if (err) {
+        console.log('An error: ', err.message);
+        return err.message;
+      }
+    }
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(info),
+  };
+};
