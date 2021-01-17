60c2dc24d5713501a4c70200658a5c8e5897e1bb
luron <joseluron@gmail.com>
2021-01-18 00:32:00 +0100

    Coded serverless 'placeOrder' function to send emails

--- functions/placeOrder/placeOrder.js
+++ functions/placeOrder/placeOrder.js
@@ -1,9 +1,29 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  return `
+  <div>
+    <h2>Your recent order for ${total}</h2>
+    <p>We will have your order readsy in the next 20 minutes</p>
+    <ul>
+      ${order
+        .map(
+          (item) => `<li>
+          <img src="${item.thumbnail}" alt="${item.name}" />
+          ${item.size} ${item.name} - ${item.price} 
+        </li>`
+        )
+        .join('')}
+    </ul>
+    <h2>Your total is ${total} due at pickup</h2>
+  </div>
+  `;
+}
+
 // Create a transport for nodemailer
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: 587,
+  port: process.env.MAIL_PORT,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
@@ -11,14 +31,28 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
-  // Test send an email
+  const body = JSON.parse(event.body);
+  // Validate the data coming in is correct
+  const requiredFields = ['email', 'name', 'order'];
+  for (const field of requiredFields) {
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Oops! You are missing the ${field} field`,
+        }),
+      };
+    }
+  }
+
+  // Send the email
   const info = await transporter.sendMail(
     {
       from: "Slick's Slices <slick@example.com>",
-      to: 'orders@example.com',
+      to: `${body.name} <${body.email}>, orders@example.com`,
       subject: 'New Order!!',
       text: 'This is your order!',
-      html: `<p>Your new pizza order is here!</p>`,
+      html: generateOrderEmail({ order: body.order, total: body.total }),
     },
     (err) => {
       if (err) {
@@ -30,6 +64,6 @@ exports.handler = async (event, context) => {
 
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success' }),
   };
 };
